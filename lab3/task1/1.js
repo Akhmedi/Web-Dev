935bb5c793c73a359db0239161804b01c017f728
Ахмеди <akhmedi@Noutbuk-Ahmedi.local>
2026-02-10 00:13:04 +0500

    Add lab3 folder

---
+++ lab3/task1/1.js
@@ -0,0 +1,21 @@
+// 1
+alert(1);
+// 2
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+// 3
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+// 4
+const BIRTHDAY = '18.04.1982'; 
+const age = someCode(BIRTHDAY); 
+// 5
+let namy = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"namy"}` ); // hello namy
+
+alert( `hello ${namy}` ); // hello Ilya
